e07dae878c69ccf3e2279989419feb7d7687b28b
Kirbstomper <WingedKraby@gmail.com>
2018-03-22 22:56:08 -0400

    Stating Simple_Creature

---
+++ Assets/Simple_Creature.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Simple_Creature : MonoBehaviour {
+   // Stats copied from chao garden
+    public int pow_stat; //Power
+    public int run_stat; //RUn
+    public int fly_stat; // Fly
+    public int sta_stat; //Stamina
+    public int swm_stat; // Swin
+    private int int_stat; // Intelligence: hidden from view
+    private int luc_stat; // luck: Hidden from view
+   
+	// Randomizes stats on creation of chao for variety/SOFT RESETS
+	void Start () {
+        pow_stat = Random.Range(1, 5);
+        run_stat = Random.Range(1, 5);
+        fly_stat = Random.Range(1, 5);
+        swm_stat = Random.Range(1, 5);
+        sta_stat = Random.Range(1, 5);
+        int_stat = Random.Range(1, 5);
+        luc_stat = Random.Range(1, 5);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
