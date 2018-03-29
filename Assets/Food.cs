2dc93c6866dc38d7daea15a2cea4d5a3f56d6f69
Kirbstomper <WingedKraby@gmail.com>
2018-03-28 21:19:31 -0400

    added hunger

--- Assets/Food.cs
+++ Assets/Food.cs
@@ -7,6 +7,7 @@ public class Food : MonoBehaviour {
     public int fly; // Fly
     public int sta; //Stamina
     public int swm; // Swin
+    public float hungerVal;
     
     void Start () {
         pow = 0;
@@ -14,6 +15,7 @@ public class Food : MonoBehaviour {
         fly = 0;
         sta = 4;
         swm = 0;
+        hungerVal = .6f;
 	}
 	
 	// Update is called once per frame
--- Assets/Simple_Creature.cs
+++ Assets/Simple_Creature.cs
@@ -10,7 +10,9 @@ public class Simple_Creature : MonoBehaviour {
     public int swm_stat; // Swin
     private int int_stat; // Intelligence: hidden from view
     private int luc_stat; // luck: Hidden from view
-   
+    public int age; // Age of creature
+    public float hungerMax;
+    public float hungedFilled;
 	// Randomizes stats on creation of chao for variety/SOFT RESETS
 	void Start () {
         pow_stat = Random.Range(1, 5);
@@ -20,17 +22,25 @@ public class Simple_Creature : MonoBehaviour {
         sta_stat = Random.Range(1, 5);
         int_stat = Random.Range(1, 5);
         luc_stat = Random.Range(1, 5);
+        age = 0;
+        hungedFilled = 0;
+        hungerMax = 1f;
     }
 	
 	// Update is called once per frame
 	void Update () {
         // Scan for food
-        Food [] foodlist = FindObjectsOfType<Food>();
-        if(foodlist.Length > 0)
+
+        if (hungedFilled < hungerMax)
         {
-            float speed = run_stat * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, foodlist[0].transform.position, speed);
-            if (transform.position == foodlist[0].transform.position) eat(foodlist[0]);
+
+            Food[] foodlist = FindObjectsOfType<Food>();
+            if (foodlist.Length > 0)
+            {
+                float speed = run_stat * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, foodlist[0].transform.position, speed);
+                if (transform.position == foodlist[0].transform.position) eat(foodlist[0]);
+            }
         }
 	}
 
@@ -41,6 +51,7 @@ public class Simple_Creature : MonoBehaviour {
         fly_stat += f.fly;
         swm_stat += f.swm;
         sta_stat += f.sta;
+        hungedFilled += f.hungerVal;
         DestroyObject(f.gameObject);
     }
 }
