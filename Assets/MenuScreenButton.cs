5f388972fce17642948ea158639456e179d40740
Kirbstomper <WingedKraby@gmail.com>
2018-04-08 17:52:16 -0400

    Working on bottom screen buttons

--- Assets/MenuScreenButton.cs
+++ Assets/MenuScreenButton.cs
@@ -6,18 +6,39 @@ using UnityEngine.EventSystems;
 public class MenuScreenButton : MonoBehaviour {
     public static GameObject menu_display;
     public Button button;
-	// Use this for initialization
-	void Start () {
+    SpriteRenderer sr;
+    // Use this for initialization
+    void Start () {
         Button btn = button.GetComponent<Button>();
-       // btn.onClick.AddListener(ChangeColour);
-	}
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        // btn.onClick.AddListener(ChangeColour);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-  public void ChangeColour()
+    /* Button test method
+     */
+  public void ChangeColour(int c)
     {
-        print("hi");
+        
+        int s = c;
+        switch (s)
+        {
+            case 0:
+                sr.color = Color.blue;
+                break;
+            case 1:
+                sr.color = Color.yellow;
+                break;
+            case 2:
+                sr.color = Color.red;
+                break;
+            default:
+                sr.color = Color.white;
+                break;
+        }
+                   
     }
 }
